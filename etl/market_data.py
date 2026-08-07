fad3a30188d9a415b1798b4e9c8bfffa933df379
Brandon <brandonnicolas827@yahoo.com>
2026-08-07 17:55:27 -0400

    Create folder structure, initial market_data.py commit

---
+++ etl/market_data.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+etfs = ['SPY', 'QQQ', 'IWM', 'VTI', 'VXUS', 'BND']
+
+data = yf.download(etfs, start='2020-01-01', end='2026-01-01')
+
+data_df = pd.DataFrame(data)
+
+df_long = data_df.stack(level="Ticker", future_stack=True).reset_index()
+
+#data_df.to_csv('C:/Users/brand/Modern-Investment-Analytics-Platform/data/ETFs.csv')
